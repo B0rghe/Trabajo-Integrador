9635eb34b50657a3e753cb32eedf168af9a71580
Alexis Borghetti <borghe.08@gmail.com>
2023-05-24 18:08:10 -0300

    Version2

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+function calcularPrecio() {
+    var cantidadTickets,
+        precioTotal,
+        categoria,
+        precioEstudiante,
+        precioTrainee,
+        precioJunior,
+        precioGeneral,
+        precio = 200;
+
+    precioEstudiante = precio * 0.2;
+    precioTrainee = precio * 0.5;
+    precioJunior = precio * 0.85;
+    precioGeneral = precio;
+
+    if (cantidadTickets > 0 && !isNaN(cantidadTickets) && cantidadTickets % 1 == 0) {
+        switch (categoria) {
+            case 'Estudiante':
+              precioTotal = precioEstudiante * cantidadTickets;
+              break;
+            case 'Trainee':
+              precioTotal = precioTrainee * cantidadTickets;
+              break;
+            case 'Junior':
+              precioTotal = precioJunior * cantidadTickets;
+              break;
+            case 'General':
+              precioTotal = precioGeneral * cantidadTickets;
+              break;
+        }
+        document.getElementById('importeTotal').innerHTML = 'Total a Pagar: $' + precioTotal;
+    } else { if (cantidadTickets == 0) {
+                window.alert('Ingrese la cantidad de entradas');
+            } else {
+                window.alert('Ingrese un numero válido');
+            }
+    }
+  }
+
+  function borrar() {
+    document.getElementById('inputNombre').value = '';
+    document.getElementById('inputApellido').value = '';
+    document.getElementById('inputEmail').value = '';
+    document.getElementById('inputCantidad').value = '';
+    document.getElementById('inputCategoria').value = 'Estudiante';
+    document.getElementById('importeTotal').innerHTML = 'Total a Pagar: $';
+  }
